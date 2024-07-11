7d4bb138f049d585e72e2f5a623bb2e1d765584c
Nilashmam <nilashmam@gmail.com>
2024-07-11 09:08:27 +0530

    register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route , BrowserRouter as Router} from 'react-router-dom'
 import Home from '../src/pages/home'
 import Register from '../src/pages/register'
 import Login from './pages/login'
-import Profile from './pages/profile'
 import Dashboard from './pages/dashboard'
 import UserContextProvider from './context/userContextProvider'
 import PrivateRoute from './components/privateRoute';
@@ -13,24 +9,29 @@ import InterviewerRoute from './components/interviewerRoute'
 import ReceiverDashboard from './pages/receiverDashboard'
 import NameInput from './pages/nameInput'
 import LinkInput from './pages/linkInput'
+
+
 function App() {
 
   return (
+    <Router>
     <UserContextProvider>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login/>} />
-        <Route element={<PrivateRoute />}>
-          <Route path="/linkInput" element={<LinkInput/>}/>
-          <Route path='/dashboard/:urlCode' element={<Dashboard />} />
-        </Route>
-        <Route element={<InterviewerRoute />}>
-          <Route path='/receiver/:urlCode/:userName' element={<ReceiverDashboard/>}/>
-          <Route path='/receiverDashboard' element={<NameInput/>}/>
-        </Route>
-      </Routes>
+      
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/linkInput" element={<LinkInput />} />
+            <Route path='/dashboard/:urlCode' element={<Dashboard />} />
+          </Route>
+          <Route element={<InterviewerRoute />}>
+            <Route path='/receiver/:urlCode/:userName' element={<ReceiverDashboard />} />
+            <Route path='/receiverDashboard' element={<NameInput />} />
+          </Route>
+        </Routes>
     </UserContextProvider>
+    </Router>
   )
 }
 
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,89 +1,177 @@
-import React, { useContext } from 'react'
-import {useState} from "react"
-import axios from 'axios'
-import {useNavigate , Navigate} from 'react-router-dom'
-import userContext from '../context/userContext'
-
+import React, { useContext } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate, Navigate } from "react-router-dom";
+import userContext from "../context/userContext";
+import {
+  Card,
+  Input,
+  Checkbox,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
 
 const register = () => {
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState({
+    username: "",
+    first_name: "",
+    last_name: "",
+    email: "",
+    password: "",
+    userType: "",
+  });
+  let { user } = useContext(userContext);
 
+  const registerUser = (e) => {
+    e.preventDefault();
+    console.log(userData);
+    axios
+      .post("https://intra-backend.onrender.com/user/register", userData)
+      .then((response) => console.log(response), navigate("/login"))
+      .catch((err) => console.log(err));
+  };
 
-    const navigate = useNavigate();
-    const [userData,setUserData] = useState({
+  return user ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <div
+      className=" flex justify-end h-full  min-h-screen px-20 py-5 bg-[url('https://images.pexels.com/photos/3746957/pexels-photo-3746957.jpeg')] bg-no-repeat bg-contain"
+      style={{ backgroundSize: "60%" }}
+    >
+      <Card color="transparent" shadow={false}>
+        <Typography variant="h4" color="blue-gray">
+          Sign Up
+        </Typography>
+        <form
+          className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96 h-full"
+          onSubmit={registerUser}
+        >
+          <div className="mb-1 flex flex-col gap-4">
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Username
+            </Typography>
+            <Input
+              size="lg"
+              placeholder="enter a valid username"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+              name="username"
+              value={userData.username}
+              onChange={(e) =>
+                setUserData({ ...userData, username: e.target.value })
+              }
+            />
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              First Name
+            </Typography>
+            <Input
+              size="lg"
+              placeholder="enter your first name"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+              name="first_name"
+              value={userData.first_name}
+              onChange={(e) =>
+                setUserData({ ...userData, first_name: e.target.value })
+              }
+            />
 
-        username : '',
-        first_name : '',
-        last_name : '',
-        email : '',
-        password : '',
-        userType : ''
-    })
-    let {user} = useContext(userContext)
-    
-    const registerUser = (e) => {
-          e.preventDefault()
-          console.log(userData)
-          axios.post('https://intra-backend.onrender.com',userData)
-          .then(
-            response => console.log(response),
-            navigate('/login')
-            )
-          .catch(err => console.log(err))
-    }
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Last Name
+            </Typography>
+            <Input
+              size="lg"
+              placeholder="enter your last name"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+              name="last_name"
+              value={userData.last_name}
+              onChange={(e) =>
+                setUserData({ ...userData, last_name: e.target.value })
+              }
+            />
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Your Email
+            </Typography>
+            <Input
+              size="lg"
+              placeholder="name@mail.com"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+              name="email"
+              value={userData.email}
+              onChange={(e) =>
+                setUserData({ ...userData, email: e.target.value })
+              }
+            />
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Password
+            </Typography>
+            <Input
+              type="password"
+              size="lg"
+              placeholder="********"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+              name="password"
+              value={userData.password}
+              onChange={(e) =>
+                setUserData({ ...userData, password: e.target.value })
+              }
+            />
+            <div className="flex items-center">
+              <Typography variant="h6" color="blue-gray" className="mr-2">
+                Candidate:
+              </Typography>
+              <input
+                type="radio"
+                id="candidate"
+                name="userType"
+                className="mr-4"
+                onClick={(e) =>
+                  setUserData({ ...userData, userType: "Candidate" })
+                }
+              />
 
-
-    return user ? (
-        <Navigate to='/dashboard'/>
-    ) : (
-        <div>
-            <div align="center">
-            <form onSubmit={registerUser}>
-                <table>
-                    <tbody>
-                    <tr>
-                        <td>
-                            UserName : <input type="text" name="username" value={userData.username} onChange={(e) => setUserData({...userData, username : e.target.value })}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            First_Name : <input type="text" name="first_name" value={userData.first_name} onChange={(e) => setUserData({...userData, first_name : e.target.value })}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Last_Name : <input type="text" name="last_name" value={userData.last_name} onChange={(e) => setUserData({...userData, last_name : e.target.value })}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Email : <input type="email" name="email" value={userData.email} onChange={(e) => setUserData({...userData, email : e.target.value })}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Password : <input type="password" name="password" value={userData.password} onChange={(e) => setUserData({...userData, password : e.target.value })}/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                           <input type="radio" id="userType" name="userType" onClick={ (e) => setUserData({...userData,userType : "Candidate"})}/>
-                           <label for="userType">Candidate</label>
-                           <input type="radio" id="userType" name="userType" onClick={ (e) => setUserData({...userData,userType : "Interviewer"})}/>
-                           <label for="userType">Interviewer</label>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <button type="submit">Submit</button>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            </form>
+              <Typography variant="h6" color="blue-gray" className="mr-2">
+                Interviewer:
+              </Typography>
+              <input
+                type="radio"
+                id="interviewer"
+                name="userType"
+                className="mr-4"
+                onClick={(e) =>
+                  setUserData({ ...userData, userType: "Interviewer" })
+                }
+              />
             </div>
-        </div>
-    );
-}
+          </div>
 
-export default register
+          <Button type="submit" className="mt-6" fullWidth>
+            sign up
+          </Button>
+          <Typography color="gray" className="mt-4 text-center font-normal">
+            Already have an account?{" "}
+            <a href="login" className="font-medium text-gray-900">
+              Sign In
+            </a>
+          </Typography>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default register;
