650230bd3942ba6e9773fe5f2b4ffe9bc8797474
Nilashmam <nilashmam@gmail.com>
2024-07-11 12:48:34 +0530

    login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,21 +1,24 @@
-import React, { useContext } from 'react'
-import { useState } from "react"
-import axios from 'axios'
-import { Navigate } from 'react-router-dom'
-import userContext from '../context/userContext'
-import {jwtDecode} from 'jwt-decode';
-import '../App.css'
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import userContext from "../context/userContext";
+import "../App.css";
+import {
+  Card,
+  Input,
+  Checkbox,
+  Button,
+  Typography,
+} from "@material-tailwind/react";
 
 const login = () => {
-    let {loginUser} = useContext(userContext)
-    let {user} = useContext(userContext)
+  let { loginUser } = useContext(userContext);
+  let { user } = useContext(userContext);
 
-    return user ? (
-        <Navigate to="/redirector" />
-    ) : (
-        <div>
-            <div align="center">
-                <form onSubmit={loginUser} className='formStyle'>
+  return user ? (
+    <Navigate to="/redirector" />
+  ) : (
+    <div className="flex">
+      {/* <form onSubmit={loginUser} className='formStyle'>
                     <table>
                         <tbody>
                             <td className='tdStyle'>
@@ -43,11 +46,65 @@ const login = () => {
 
                         </tbody>
                     </table>
-                </form>
-            </div>
-        </div>
-    )
+                </form> */}
 
+        <div style={{marginTop: '8%' , padding : '5%'}}>
+        <Card color="transparent" shadow={false}>
+          <Typography variant="h4" color="blue-gray">
+            Sign In
+          </Typography>
+          <form
+            className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96 h-full"
+            onSubmit={loginUser}
+          >
+            <div className="mb-1 flex flex-col gap-4">
+              <Typography variant="h6" color="blue-gray" className="-mb-3">
+                Username
+              </Typography>
+              <Input
+                size="lg"
+                placeholder="enter a valid username"
+                className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                labelProps={{
+                  className: "before:content-none after:content-none",
+                }}
+                name="username"
+              />
+
+              <Typography variant="h6" color="blue-gray" className="-mb-3">
+                Password
+              </Typography>
+              <Input
+                type="password"
+                size="lg"
+                placeholder="********"
+                className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                labelProps={{
+                  className: "before:content-none after:content-none",
+                }}
+                name="password"
+              />
+            </div>
+
+            <Button type="submit" className="mt-6" fullWidth>
+              sign up
+            </Button>
+            <Typography color="gray" className="mt-4 text-center font-normal">
+              Don't have an account?{" "}
+              <a href="register" className="font-medium text-gray-900">
+                Sign Up
+              </a>
+            </Typography>
+          </form>
+        </Card>
+        </div>
+        <div
+        className="min-h-screen px-20 py-5 bg-[url('https://images.pexels.com/photos/2041637/pexels-photo-2041637.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-no-repeat bg-cover"
+        style={{width:'70%'}}
+      >
+      </div>
+    </div>
+  );
 };
 
-export default login
+export default login;
